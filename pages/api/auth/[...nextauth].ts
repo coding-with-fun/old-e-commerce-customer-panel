39fd0bd6c311685f021a92beafc7563261ddc847
Harrsh Patel <dev.harrsh@gmail.com>
2023-04-28 17:15:08 +0530

    Tweaks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,13 +7,11 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
-    // Configure one or more authentication providers
     providers: [
         GithubProvider({
             clientId: env.github.clientId,
             clientSecret: env.github.clientSecret,
         }),
-        // ...add more providers here
         CredentialsProvider({
             name: 'Credentials',
             credentials: {
@@ -24,10 +22,7 @@ export const authOptions: NextAuthOptions = {
                 password: { label: 'Password', type: 'password' },
             },
             authorize: async (credentials) => {
-                // Add logic here to look up the user from the credentials supplied
                 await connectMongo();
-                console.log(credentials);
-
                 const email = _.get(credentials, 'email', '');
                 const password = _.get(credentials, 'password', '');
 
@@ -38,41 +33,11 @@ export const authOptions: NextAuthOptions = {
                     throw new Error('Customer does not exist.');
                 }
 
-                console.log(customer);
-
                 return {
                     email,
                     password,
-                    id: '1',
+                    id: customer._id,
                 };
-
-                // const email = _.get(credentials, 'email', '');
-                // const password = _.get(credentials, 'password', '');
-
-                // const customer = await Customer.findOne({
-                //     email,
-                // });
-                // if (!customer) {
-                //     throw new Error('Customer does not exist.');
-                // }
-
-                // console.log(customer);
-
-                // return {
-                //     id: customer.customerID,
-                //     email: customer.email,
-                //     password: customer.password,
-                // };
-
-                // if (user) {
-                //     // Any object returned will be saved in `user` property of the JWT
-                //     return user;
-                // } else {
-                //     // If you return null then an error will be displayed advising the user to check their details.
-                //     return null;
-
-                //     // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                // }
             },
         }),
     ],
