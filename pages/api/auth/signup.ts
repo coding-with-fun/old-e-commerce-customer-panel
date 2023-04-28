ece171144e1bb555046699e10d9d54529aec92d2
Harrsh Patel <dev.harrsh@gmail.com>
2023-04-28 14:57:01 +0530

    Added API request checker

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -2,9 +2,21 @@ import connectMongo from '@/database/conn';
 import _ from 'lodash';
 import mongoose from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
+        const data = schema.safeParse(req);
+
+        if (!data.success) {
+            const message = _.get(
+                data,
+                'error.issues[0].message',
+                'Something went wrong.'
+            );
+            throw new Error(message);
+        }
+
         await connectMongo();
 
         return res.status(200).json({ name: 'John Doe' });
@@ -20,3 +32,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 export default handler;
+
+const schema = z.object({
+    body: z.object({
+        email: z
+            .string({
+                required_error: 'Email address is required.',
+                invalid_type_error: 'Email address is required.',
+            })
+            .email('Invalid email address.'),
+    }),
+});
